b7caca2d3bf5d503ec3b840a4af9dabf53d42b48
giuliogallerini <giulio.gallerini.work.com>
2016-10-11 20:48:11 +0200

    fix bug db, intial seed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161011155416) do
+ActiveRecord::Schema.define(version: 20161011184503) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,13 @@ ActiveRecord::Schema.define(version: 20161011155416) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "categories_products", id: false, force: :cascade do |t|
+    t.integer "product_id"
+    t.integer "category_id"
+    t.index ["category_id"], name: "index_categories_products_on_category_id", using: :btree
+    t.index ["product_id"], name: "index_categories_products_on_product_id", using: :btree
+  end
+
   create_table "products", force: :cascade do |t|
     t.string   "name"
     t.decimal  "price"
@@ -28,14 +35,8 @@ ActiveRecord::Schema.define(version: 20161011155416) do
     t.integer  "user_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "image"
     t.index ["user_id"], name: "index_products_on_user_id", using: :btree
-  end
-
-  create_table "products_categories", id: false, force: :cascade do |t|
-    t.integer "product_id"
-    t.integer "categories_id"
-    t.index ["categories_id"], name: "index_products_categories_on_categories_id", using: :btree
-    t.index ["product_id"], name: "index_products_categories_on_product_id", using: :btree
   end
 
   create_table "users", force: :cascade do |t|
@@ -52,9 +53,11 @@ ActiveRecord::Schema.define(version: 20161011155416) do
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.string   "role"
+    t.string   "name"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  add_foreign_key "categories_products", "categories"
   add_foreign_key "products", "users"
 end
