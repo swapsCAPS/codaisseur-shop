ec6b623ca4de8dd196e8c3f04e2d91e99e1379d9
MidnightP <heijden.pvd@gmail.com>
2016-10-12 23:52:59 +0200

    feature test not working yet

--- spec/factories/products.rb
+++ spec/factories/products.rb
@@ -1,27 +1,19 @@
 FactoryGirl.define do
 
   factory :product do
-    #home_type         "House"
-    #room_type         "Shared"
-    #accommodate       2
-    #bedroom_count     2
-    #bathroom_count    3
-    #listing_name      { Faker::Lorem.sentence(2) }
+    # name      { Faker::Pokemon.name }
+
     #description       { Faker::Lorem.sentence(40) }
     #address           { Faker::Address.city }
-    #has_tv            true
-    #has_kitchen       true
-    #has_airco         true
-    #has_heating       true
-    #has_internet      true
+
     #price             { Faker::Commerce.price }           { Faker::Commerce.price }
 
-    trait :active do
-      active true
-    end
+    #trait :active do
+    #  active true
+    #end
 
-    trait :inactive do
-      active false
-    end
+    #trait :inactive do
+    #  active false
+    #end
   end
 end
--- spec/features/show_categories_spec.rb
+++ spec/features/show_categories_spec.rb
@@ -6,18 +6,55 @@ describe "Navigating" do
 
   # before { login_as user }        #these pages are identical for all users
 
-  let(:user) { create :user }
-  let!(:category) { create :category }
-  let!(:product) { create :product, user: user, category: category }
+  let!(:user) { create :user }
+  let!(:category) { create :category, name: "Nuts" }
+  let!(:product) { create :product, name: "Peanut", user: user, categories: [category] }
 
-  context "from category/index page to the category/show" do
-    it "is category/show page" do
+  context "from category/index page to category/show to product/show and back to category/show" do
+    it "is category/show page and product/show and category/show" do
 
       visit categories_url
 
+          # forth
+      click_link "Nuts"
+
+      expect(current_path).to eq(category_path(category.id))
+
+          # forth
+      click_link "Peanut"
+
+          # why can't we use the random names automatically created by the factory? --> want to use click_link category.name and product.name...
+          # ... its gives error "ActionView::Template::Error: nil is not a valid asset source"
+
+      expect(current_path).to eq(product_path(product))
+
+          # and back once
+      #click_link category.name
+
+      #expect(current_path).to eq(category_path(category))
+    end
+  end
+
+  context "product/show -> category/show" do
+    it "" do
+
+      visit product_path(product)
+
+      expect(current_path).to eq(product_path(product))
+
       click_link category.name
 
-      expect(current_path).to eq(category_path)
+      expect(current_path).to eq(category_path(category))
+    end
+  end
+
+  context "product/show is there? " do
+    it "" do
+
+      visit product_path(product)
+
+      expect(current_path).to eq(product_path(product))
+
     end
   end
 end
