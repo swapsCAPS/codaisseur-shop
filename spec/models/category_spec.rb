55cf68fc6245aeeb9221889feb289dc466b3e114
MidnightP <heijden.pvd@gmail.com>
2016-10-12 21:39:12 +0200

    added similar tests to category_spec and association of product with user

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,5 +1,28 @@
 require 'rails_helper'
 
+# here we test our product-category relation in 2 ways (see product_spec.rb for 2 more)
+# 1 category contains 1 product
+# 1 category contains multiple products
+
 RSpec.describe Category, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "category has one product" do
+    let(:user1) {create :user}
+    let(:product1) { create :product, user: user1 }
+    let(:category1) { create :category, products: [product1] }      # same remark as above: we need an array
+
+    it "has category" do
+      expect(category1.products).to eq([product1])
+    end
+  end
+
+  context "category has multiple products" do
+    let(:user1) {create :user}
+    let(:product1) { create :product, user: user1 }
+    let(:product2) { create :product, user: user1 }
+    let(:category1) { create :category, products: [product1, product2] }
+
+    it "has category" do
+      expect(category1.products).to eq([product1, product2])
+    end
+  end
 end
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,37 +1,44 @@
 require 'rails_helper'
 
 
-# here we test our product-category relation in 4 ways
+# here we test our product-category relation in 2 ways (see category_spec.rb for 2 more)
 # 1 product has 1 category
 # 1 product has multiple categories
-# 1 category contains 1 product
-# 1 category contains multiple products
+
+# we also check the association of a product with a user
 
 RSpec.describe Product, type: :model do
 
-    context "product has category" do
+  describe "association with user" do               # a product needs a user
+    let(:user) { create :user }
 
-      let(:user1) {create :user}
-      let(:category1) { create :category }
-      let(:product1) { create :product, user: user1, categories: [category1] }    # one product has many categories, so we need an array categories: [category1]
+    it "belongs to a user" do
+      product = user.products.new()
 
-      it "has category" do
-        expect(product1.categories).to eq([category1])
-      end
+      expect(product.user).to eq(user)
     end
+  end
 
-    context "product has multiple categories" do
+  context "product has one category" do
 
-      let(:user1) {create :user}
-      let(:category1) { create :category }
-      let(:category2) { create :category }
-      let(:product1) { create :product, user: user1, categories: [category1, category2] }
+    let(:user1) {create :user}
+    let(:category1) { create :category }
+    let(:product1) { create :product, user: user1, categories: [category1] }    # one product has many categories, so we need an array categories: [category1]
 
-      it "has category" do
-        expect(product1.categories).to eq([category1, category2])
-      end
+    it "has category" do
+      expect(product1.categories).to eq([category1])
     end
+  end
 
-    context "product has multiple categories" do
+  context "product has multiple categories" do
 
+    let(:user1) {create :user}
+    let(:category1) { create :category }
+    let(:category2) { create :category }
+    let(:product1) { create :product, user: user1, categories: [category1, category2] }
+
+    it "has category" do
+      expect(product1.categories).to eq([category1, category2])
+    end
+  end
 end
