721730d09ce23d5ac8179ede010bc27b96b37c70
MidnightP <heijden.pvd@gmail.com>
2016-10-13 16:32:51 +0200

    reverted changes I made in master

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,6 @@
 class Category < ApplicationRecord
   has_and_belongs_to_many :products
+
+
+
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,6 @@ class Product < ApplicationRecord
   belongs_to :user
   has_and_belongs_to_many :categories
 
+
+
 end
