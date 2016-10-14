6d1cdedaecea1d4c9d76a4601d89cc1a909a88e8
giuliogallerini <giulio.gallerini.work.com>
2016-10-14 17:16:29 +0200

    add shopping cart

--- app/controllers/shopping_carts_controller.rb
+++ app/controllers/shopping_carts_controller.rb
@@ -12,11 +12,25 @@ class ShoppingCartsController < ApplicationController
   end
 
   def edit
-    product = Product.find(params[:product_id])
+    product = Product.find(params[:id])
+
     amount = params[:amount]
-    session[:shopping_cart][:products.to_s] << product
-    session[:shopping_cart][:amounts.to_s] << amount
+    res = session[:shopping_cart][:products.to_s].select{|p| p[:id] == product[:id]}
+    if  res != []
+
+      session[:shopping_cart][:products.to_s].each_with_index do |p, i|
+        if p[:name] == product[:name]
+          session[:shopping_cart][:amounts.to_s][i] += amount
+        end
+      end
+    else
+      session[:shopping_cart][:products.to_s] << product
+      session[:shopping_cart][:amounts.to_s] << amount
+    end
     redirect_to product_path(product), notice: "Product successfully added to the Shopping cart"
   end
 
+  def show
+    @shopping_cart = {products: session[:shopping_cart][:products.to_s], amounts: session[:shopping_cart][:amounts.to_s]}
+  end
 end
