f14e4334879189b9b69c90c185838e88d1a83b62
giuliogallerini <giulio.gallerini.work.com>
2016-10-14 14:45:32 +0200

    add shopping cart

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,4 +6,8 @@ class ProductsController < ApplicationController
   def show
     @product = Product.find(params[:id])
   end
+
+  def edit
+    @product = Product.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,9 @@ Rails.application.routes.draw do
 
   root to: 'categories#index'
 
-  resources :products
-
+  resources :products do
+    resources :shopping_carts
+  end
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
