e546cb18ca553869ed962b245172dea6181b1a0e
giuliogallerini <giulio.gallerini.work.com>
2016-10-16 22:39:20 +0200

    change style product show

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,14 @@
 //= require turbolinks
 //= require_tree .
 //= require bootstrap-sprockets
+
+$(function(){
+
+    $('#show').on('click',function(){
+        $('.card-reveal').slideToggle('slow');
+    });
+
+    $('.card-reveal .close').on('click',function(){
+        $('.card-reveal').slideToggle('slow');
+    });
+});
