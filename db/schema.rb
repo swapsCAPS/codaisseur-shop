80ded43437916b0f60d261b7549d3128486514ef
Robert Hopman <hopman.r@gmail.com>
2016-10-18 15:46:08 +0200

    remove bug from schema

--- db/schema.rb
+++ db/schema.rb
@@ -79,28 +79,6 @@ ActiveRecord::Schema.define(version: 20161018132622) do
     t.index ["user_id"], name: "index_profiles_on_user_id", using: :btree
   end
 
-  create_table "rooms", force: :cascade do |t|
-    t.string   "home_type"
-    t.string   "room_type"
-    t.integer  "accommodate"
-    t.integer  "bedroom_count"
-    t.integer  "bathroom_count"
-    t.string   "listing_name"
-    t.text     "description"
-    t.string   "address"
-    t.boolean  "has_tv"
-    t.boolean  "has_kitchen"
-    t.boolean  "has_airco"
-    t.boolean  "has_heating"
-    t.boolean  "has_internet"
-    t.decimal  "price"
-    t.boolean  "active"
-    t.integer  "user_id"
-    t.datetime "created_at",     null: false
-    t.datetime "updated_at",     null: false
-    t.index ["user_id"], name: "index_rooms_on_user_id", using: :btree
-  end
-
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
