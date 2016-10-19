22f984888bbdb36bff5bdc5f6ef8bb5bb98cf4c7
stofstik <verstegen.daan@gmail.com>
2016-10-19 15:44:25 +0200

    added shopping cart and order styling

--- app/controllers/shopping_carts_controller.rb
+++ app/controllers/shopping_carts_controller.rb
@@ -52,12 +52,11 @@ class ShoppingCartsController < ApplicationController
 
   def destroy
     if session[:shopping_cart]
-      if params[:id] && params[:id] != 0
+      if params[:id] && params[:id].to_i != 0
         session[:shopping_cart].except!(params[:id])
         redirect_to shopping_carts_path, notice: "Product successfully removed to the shopping cart"
       else
         session[:shopping_cart] = {}
-        redirect_to shopping_carts_path
         redirect_to shopping_carts_path, notice: "Shopping cart is now empty"
       end
     end
