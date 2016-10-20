0d4d09e7d49601af8aeebf3654d0afc66a7f1889
giuliogallerini <giulio.gallerini.work.com>
2016-10-20 09:39:31 +0200

    add profile picture

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,10 +1,12 @@
 class Profile < ApplicationRecord
+  mount_uploader :photo, ProfileUploader
 
   belongs_to :user
 
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :bio, presence: true
+  validates :photo, presence: true
 
   def full_name
     "#{first_name} #{last_name}"
