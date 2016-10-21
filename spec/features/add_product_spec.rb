ceb02c5d005de5a04912503e6cb74040a2ba07f2
stofstik <verstegen.daan@gmail.com>
2016-10-21 12:22:16 +0200

    Added product feature test

--- spec/features/add_product_spec.rb
+++ spec/features/add_product_spec.rb
@@ -2,18 +2,36 @@ require 'rails_helper'
 require 'capybara'
 
 feature 'Add product', js: true do
-    before { login_as user }
+  # user needs to be logged in to add a product
+  before { login_as user }
 
-    let ( :user) { create :user, email: "host@user.com" }
-    let! ( :photo ) { create :photo, image: "bla" }
-    let! ( :category ) { create :category, name: "A category", products: [] }
+  # create a user
+  let ( :user) { create :user, email: "host@user.com" }
+  # create a tempt category
+  let! ( :category ) { create :category, name: "A category", products: [] }
+
   scenario 'add a product' do
-
     visit new_product_path
 
+    # fill in the form
     fill_in 'product_name', with: 'Automatic website builder'
     fill_in 'product_description', with: 'Teh awesome description'
     fill_in 'product_price', with: 10
+
+    within '#product_category_ids' do
+      find("option[value='#{category.id}']").click
+    end
+
+    attach_file('images_', File.join(Rails.root, '/spec/support/IMG-20160920-WA0005.jpg'))
+
+    # click save
+    click_button('Save')
+
+    sleep(5)
+
+    # expect to have a product in the db now
+    expect(Product.all.length).to eq(1)
+
 
   end
 end
